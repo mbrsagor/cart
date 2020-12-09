33868ed3643dbc1f3998f10d072324eff4d7ca75
Sagor <mbrsagor@gmail.com>
2020-12-09 18:02:48 +0600

    Add new cart class in index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+class Cart {
+    constructor(name, product_name, price) {
+        this.name = name;
+        this.product_name = product_name;
+        this.price = price;
+    }
+
+    all() {
+        const config = `Car name is: ${this.name}\nProduct name:${this.product_name}\nPrice:${this.price}`;
+        console.log(config);
+    }
+}
+const my_car = new Cart('something', 'something 2', 129393);
+my_car.all();
+
