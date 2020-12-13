0f73959410c8f56e95a1249016443e9ac412e52b
Sagor <mbrsagor@gmail.com>
2020-12-13 15:45:54 +0600

    Seeder box show empty data

--- app/routes/admin/seeder.js
+++ app/routes/admin/seeder.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import { hash } from "rsvp";
 
 export default Route.extend({
+//     models() {
+//         return hash({
+//           libraries: this.store.findAll("library"),
+//           books: this.store.findAll("book"),
+//           authors: this.store.findAll("author"),
+//         });
+//     },
+//     setupController(controller, model) {
+//     controller.set('libraries', model.libraries);
+//     controller.set('books', model.books);
+//     controller.set('authors', model.authors);
+    
+//     this._super(controller, model);
+//   }
 });
