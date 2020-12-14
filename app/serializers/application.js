cfaca2e097476723ee21f5a4d9d3c017958b1d51
Sagor <mbrsagor@gmail.com>
2020-12-14 17:28:51 +0600

    Add new seralizers for post data problem fixed

---
+++ app/serializers/application.js
@@ -0,0 +1,24 @@
+import DS from "ember-data";
+
+export default DS.JSONAPISerializer.extend({
+  serialize: function (snapshot /*, options*/) {
+    let json = this._super(...arguments);
+    json.data.type = Ember.String.capitalize(snapshot.modelName);
+    json.data.id = snapshot.id;
+    if (json.data.relationships) {
+      json.data.attributes = {
+        title: snapshot._attributes.title,
+        release: new Date(snapshot._attributes.release)
+          .toISOString()
+          .slice(0, 10),
+        library: json.data.relationships.library.data.id,
+        author: json.data.relationships.author.data.id,
+      };
+
+      delete json.data.relationships;
+    } else {
+      json.data.attributes = snapshot._attributes;
+    }
+    return json;
+  },
+});
