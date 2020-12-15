1a9a484e5479003d928dbce2905e24ed1a761033
Sagor <mbrsagor@gmail.com>
2020-12-15 22:17:49 +0600

    Remove backend

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.store.createRecord("contact");
+  },
+
+  actions: {
+    sendMessage() {
+        
+    }
+  }
 });
--- backend/books/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Library, Author, Book, Invitations
-
-admin.site.register(Library)
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Invitations)
