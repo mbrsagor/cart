5d8a93a81291620ed730ae08882eba6d06d424f2
Sagor <mbrsagor@gmail.com>
2020-12-15 11:31:48 +0600

    Books show and editable

--- app/models/author.js
+++ app/models/author.js
@@ -1,20 +1,16 @@
 import DS from 'ember-data';
-const { Model, attr, hasMany } = DS;
+const { Model} = DS;
+import { empty } from "@ember/object/computed";
 import Faker from "faker";
-
 
 export default Model.extend({
   name: DS.attr("string"),
   books: DS.hasMany("book", { inverse: "author", async: true }),
 
-  // isNotValid: empty("name"),
+  isNotValid: empty("name"),
 
   randomize() {
     this.set("name", Faker.name.findName());
-
-    // With returning the author instance, the function can be chainable,
-    // for example `this.store.createRecord('author').randomize().save()`,
-    // check in Seeder Controller.
     return this;
   }
 });
--- app/routes/authors.js
+++ app/routes/authors.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  models() {
+  model() {
     return this.store.findAll("author");
   },
 
