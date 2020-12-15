ff14916ff4d8d07edbdb97fc2adabc4a8bb8d2e2
Sagor <mbrsagor@gmail.com>
2020-12-15 23:57:25 +0600

    Invaition conflig post request

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,24 +1,21 @@
 import Controller from '@ember/controller';
-import { match, not } from "@ember/object/computed";
 
 export default Controller.extend({
   responseMessage: "",
   emailAddress: "",
-  headerMessage : "Comming Soon",
-  headerSubTitle : "Don't miss our launch date, request an invitation now.",
+  headerMessage: "Comming Soon",
+  headerSubTitle: "Don't miss our launch date, request an invitation now.",
 
-  isValid: match("emailAddress", /^.+@.+\..+$/),
-  isDisabled: not("isValid"),
-  
   actions: {
-    saveInvitation(){
-        const email = this.get("emailAddress");
-        
-        const newInvitation = this.store.createRecord('invitation', { email: email });
-        newInvitation.save().then(response=> {
-            this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
-            this.set("emailAddress", "");
+    saveInvitation(newInvitation) {
+      newInvitation
+        .save()
+        .then((response) => {
+          this.set("responseMessage", true);
+          this.set("model.email", "");
+          console.log(response)
         })
-    }
-  }
+        .catch((response) => console.log("error:", response));
+    },
+  },
 });
